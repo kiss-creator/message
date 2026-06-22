39a8fdf84bff5c5d29eb13f50bca068b7c92536f
zde <tripoldought@gmail.com>
2026-06-22 14:39:46 +0600

    Create learn.c

---
+++ learn.c
@@ -0,0 +1,10 @@
+Master Basic C Programming : Made by me <-date : June 22,26 |2:38 P.M|
+
+Variables and data types
+Operators: arithmetic,relational,logical
+control flow
+arrays
+functions
+file handle
+pointer and memory management 
+Data structure and Algorithm 
